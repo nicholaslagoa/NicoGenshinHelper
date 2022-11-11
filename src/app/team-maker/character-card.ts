b80e689097a09a0445e0d54e888c4d35ffa6e53c
nicholaslagoa <nicholas.lagoa@hotmail.com>
2022-11-11 17:53:18 -0300

    font size equality continue, team-maker start

---
+++ src/app/team-maker/character-card.ts
@@ -0,0 +1,7 @@
+export class CharacterCard{
+    constructor(
+        public name: string,
+        public photo: string,
+        public selected: boolean = false
+    ){}
+}
--- src/app/team-maker/team-maker.component.ts
+++ src/app/team-maker/team-maker.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Console } from 'console';
+import { CharacterCard } from './character-card';
 
 @Component({
   selector: 'app-team-maker',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamMakerComponent implements OnInit {
 
+  cardList = [
+    new CharacterCard('Bennett', '../../assets/png/bennett.png'),
+    new CharacterCard('Xiangling', '../../assets/png/xiangling.png'),
+    new CharacterCard('Beidou', '../../assets/png/beidou.png'),
+    new CharacterCard('Childe', '../../assets/png/childe.png'),
+    new CharacterCard('Fischl', '../../assets/png/fischl.png'),
+    new CharacterCard('Zhongli', '../../assets/png/zhongli.png'),
+    new CharacterCard('Xingqiu', '../../assets/png/xingqiu.png'),
+  ];
+
+  selectedCharacters: CharacterCard[] = [];
+
+  status: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addToSelectedList(card : CharacterCard){
+    card.selected = !card.selected;
+    if (card.selected){
+      this.selectedCharacters.push(card);
+    }
+    else{
+      this.selectedCharacters.forEach((selectedCharacter, index) =>{
+        if (selectedCharacter.name == card.name) this.selectedCharacters.splice(index,1);
+      })
+    }
+    console.log(this.selectedCharacters);
+  }
 }
