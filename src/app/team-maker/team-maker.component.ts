71c38fc372c481d431998c40b00f999d0053aa0e
nicholaslagoa <nicholas.lagoa@hotmail.com>
2022-11-16 17:55:56 -0300

    added fixed units

--- src/app/team-maker/team-maker.component.ts
+++ src/app/team-maker/team-maker.component.ts
@@ -10,16 +10,21 @@ import { CharacterCard } from './character-card';
 export class TeamMakerComponent implements OnInit {
 
   cardList = [
+    new CharacterCard('Traveler', '../../assets/png/traveler.png', true),//png size = 208x248
+    new CharacterCard('Amber', '../../assets/png/amber.png', true),
+    new CharacterCard('Kaeya', '../../assets/png/kaeya.png', true),
+    new CharacterCard('Lisa', '../../assets/png/lisa.png', true),
+    new CharacterCard('Noelle', '../../assets/png/noelle.png'),
     new CharacterCard('Bennett', '../../assets/png/bennett.png'),
     new CharacterCard('Xiangling', '../../assets/png/xiangling.png'),
     new CharacterCard('Beidou', '../../assets/png/beidou.png'),
     new CharacterCard('Childe', '../../assets/png/childe.png'),
     new CharacterCard('Fischl', '../../assets/png/fischl.png'),
     new CharacterCard('Zhongli', '../../assets/png/zhongli.png'),
-    new CharacterCard('Xingqiu', '../../assets/png/xingqiu.png'),
+    new CharacterCard('Xingqiu', '../../assets/png/xingqiu.png')
   ];
 
-  selectedCharacters: string[] = [];
+  selectedCharacters: string[] = ['Traveler', 'Amber', 'Kaeya', 'Lisa'];
 
   status: boolean = false;
 
@@ -29,16 +34,25 @@ export class TeamMakerComponent implements OnInit {
   }
 
   addToSelectedList(card : CharacterCard){
-    card.selected = !card.selected;
-    if (card.selected){
-      this.selectedCharacters.push(card.name);
+    if (card.name != 'Traveler' &&
+      card.name != 'Kaeya' &&
+      card.name != 'Lisa' &&
+      card.name != 'Amber')
+    {
+      card.selected = !card.selected;
+
+      if (card.selected){
+        this.selectedCharacters.push(card.name);
+      }
+
+      else{
+        this.selectedCharacters.forEach((selectedCharacterName, index) =>{
+          if (selectedCharacterName == card.name) this.selectedCharacters.splice(index,1);
+        });
+      }
+
+      console.log(this.selectedCharacters);
     }
-    else{
-      this.selectedCharacters.forEach((selectedCharacterName, index) =>{
-        if (selectedCharacterName == card.name) this.selectedCharacters.splice(index,1);
-      })
-    }
-    console.log(this.selectedCharacters);
   }
 
   getTeams(){
